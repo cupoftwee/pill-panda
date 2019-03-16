7744f8f906b2a57213befd6ebc16118fe3c668b3
Sarah Robin <cupoftwee@Pearl13.local>
2019-03-15 19:41:30 -0700

    whoa, vue ui is p cool

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,122 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
+  <div id="app">
+    <v-app id="inspire">
+      <v-navigation-drawer
+        fixed
+        v-model="drawerRight"
+        right
+        clipped
+        app
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>fas fa-external-link-alt</v-icon>
-      </v-btn>
-    </v-toolbar>
+        <v-list dense>
+          <v-list-tile @click.stop="right = !right">
+            <v-list-tile-action>
+              <v-icon>exit_to_app</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Open Temporary Drawer</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
 
-    <v-content>
-      <router-view/>
-    </v-content>
-  </v-app>
+      <v-toolbar
+        color="blue-grey"
+        dark
+        fixed
+        app
+        clipped-right
+      >
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+
+        <v-toolbar-title>Toolbar</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+      </v-toolbar>
+
+      <v-navigation-drawer
+        fixed
+        temporary
+        v-model="drawer"
+        class="blue lighten-3"
+        dark
+        app
+      >
+        <v-list>
+          <v-list-tile
+            v-for="item in items"
+            :key="item.title"
+          >
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-navigation-drawer
+        temporary
+        v-model="left"
+        fixed
+      />
+
+      <v-content>
+        <v-container fluid fill-height>
+          <v-layout justify-center align-center>
+            <v-flex shrink>
+              <v-tooltip right>
+                <v-btn
+                  icon
+                  large
+                  :href="source"
+                  target="_blank"
+                  slot="activator"
+                >
+                  <v-icon large>code</v-icon>
+                </v-btn>
+
+                <span>Source</span>
+              </v-tooltip>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-content>
+
+      <v-footer color="blue-grey" class="white--text" app>
+        <span>Vuetify</span>
+
+        <v-spacer></v-spacer>
+
+        <span>&copy; 2017</span>
+      </v-footer>
+    </v-app>
+  </div>
 </template>
 
 <script>
 
 export default {
   name: 'App',
-  data () {
-    return {
-      //
-    }
+  data: () => ({
+    drawer: false,
+    drawerRight: false,
+    items: [
+      { title: 'Dashboard', icon: 'dashboard' },
+      { title: 'Account', icon: 'account_box' },
+      { title: 'Admin', icon: 'gavel' }
+    ],
+    right: null,
+    left: null
+  }),
+
+  props: {
+    source: String
   }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,19 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  publicPath: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+
+  css: {
+    sourceMap: true
+  },
+
+  pwa: {
+    name: 'pillpanda',
+    themeColor: '#F59EB3',
+    msTileColor: '#DA6CCA'
+  }
 }
